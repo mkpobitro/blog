e8d729bbaae894609976ac3c07b91d93907dee38
Pobitro Mondal <pobitrombd@gmail.com>
2023-04-05 00:56:07 +0600

    Pobitro blog

---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Bloggy - Personal Blog Template</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Free Website Template" name="keywords">
+        <meta content="Free Website Template" name="description">
+
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300;400;600;700;800&display=swap" rel="stylesheet">
+
+        <!-- Font Awesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="wrapper">
+            <div class="sidebar">
+                <div class="sidebar-text d-flex flex-column h-100 justify-content-center text-center">
+                    <img class="mx-auto d-block w-75 bg-primary img-fluid rounded-circle mb-4 p-3" src="img/profile.jpg" alt="Image">
+                    <h2 class="font-weight-bold">Pobitro Mondal</h2>
+                    <p class="mb-4">
+                        An Experienced Web Developer with 4+ years of working experience over on this platform. Well-versed innumerous programming languages PHP (WordPress + Laravel), JavaScript (jQuery), Structured language (HTML5, CSS3, Bootstrap) with MySql Database
+                    </p>
+                    <div class="d-flex justify-content-center mb-5">
+                        <a target="__blank" class="btn btn-outline-primary mr-2" href="https://twitter.com/mkpobitro"><i class="fab fa-twitter"></i></a>
+                        <a target="__blank" class="btn btn-outline-primary mr-2" href="https://fb.com/mkpobitro"><i class="fab fa-facebook-f"></i></a>
+                        <a target="__blank" class="btn btn-outline-primary mr-2" href="https://www.linkedin.com/in/mkpobitro/"><i class="fab fa-linkedin-in"></i></a>
+                        <a target="__blank" class="btn btn-outline-primary mr-2" href="https://www.instagram.com/mkpobitro/"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <a target="__blank" href="https://fiverr.com/pobitro_bd" class="btn btn-lg btn-block btn-primary mt-auto">Hire Me</a>
+                </div>
+                <div class="sidebar-icon d-flex flex-column h-100 justify-content-center text-right">
+                    <i class="fas fa-2x fa-angle-double-right text-primary"></i>
+                </div>
+            </div>
+            <div class="content">
+                <!-- Navbar Start -->
+                <div class="container p-0">
+                    <nav class="navbar navbar-expand-lg bg-secondary navbar-dark">
+                        <a href="" class="navbar-brand d-block d-lg-none">Navigation</a>
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                            <div class="navbar-nav m-auto">
+                                <a href="index.php" class="nav-item nav-link active">Home</a>
+                                <a href="about.php" class="nav-item nav-link">About</a>
+                                <a href="blog.php" class="nav-item nav-link">Blog</a>
+                                <a href="contact.php" class="nav-item nav-link">Contact</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <!-- Navbar End -->
+                
